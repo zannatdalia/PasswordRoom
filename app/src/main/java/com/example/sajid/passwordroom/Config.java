ed8e467154946381c8c3075a736e1327b652901f
zannatdalia <zannatdalia@gmail.com>
2017-09-09 17:15:39 +0600

    first commit

---
+++ app/src/main/java/com/example/sajid/passwordroom/Config.java
@@ -0,0 +1,13 @@
+package com.example.sajid.passwordroom;
+
+/**
+ * Created by Sajid on 9/9/2017.
+ */
+public class Config {
+
+    public static final String DATA_URL = "http://192.168.100.165/android/includes/getData.php";
+    public static final String KEY_NAME = "user_name";
+    public static final String KEY_ADDRESS = "password_type";
+    public static final String KEY_VC = "password";
+    public static final String JSON_ARRAY = "result";
+}
